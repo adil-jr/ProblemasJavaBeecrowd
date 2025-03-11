6deff8a73da8b0633951d78d070409677d0ab243
adil-jr <adil.jr@outlook.com>
2025-03-11 15:08:56 -0300

    feat: adicionar soluções iniciais para os problemas de repetição (BEE1070)

---
+++ Problemas_Iniciante_Repeticao/BEE1070.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class BEE1070 {
+    public static void main (String args[]){
+        Scanner teclado = new Scanner(System.in);
+        int inicio = teclado.nextInt();
+
+        if (inicio % 2 == 0){
+            inicio++;
+        }
+
+        for (int cont = 1; cont <= 6; cont++){
+            System.out.println(inicio);
+            inicio += 2;
+        }
+
+
+        teclado.close();
+    }
+}
