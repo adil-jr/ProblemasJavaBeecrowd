e788d0e39ca0ce3494791cdcecc0cafb615e0ac4
adil-jr <adil.jr@outlook.com>
2025-03-13 18:33:03 -0300

    feat: adicionar soluções iniciais para os problemas de repetição (BEE1059)

---
+++ Problemas_Iniciante_Repeticao/BEE1059.java
@@ -0,0 +1,8 @@
+public class BEE1059 {
+    public static void main (String args[]) {
+
+        for (int cont = 2; cont <= 100; cont+=2) {
+            System.out.println(cont);
+        }
+    }
+}
